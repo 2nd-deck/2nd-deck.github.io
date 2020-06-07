dd12e2b0ed7f1f0e099ee9a65f8e805225f6791f
2nd-deck <61179880+2nd-deck@users.noreply.github.com>
2020-06-07 12:03:07 +0900

    d

---
+++ _drafts/stock.py
@@ -0,0 +1,9 @@
+# 설치
+* Pycharm : https://www.jetbrains.com/Pycharm
+* Anaconda3 : https://www.anaconda.com/download
+* tensorflow, Keras : pip install tensorflow Keras
+
+#세팅
+* Anaconda Prompt
+    - Python 실행
+    - import Keras
